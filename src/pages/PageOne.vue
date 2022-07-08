2e3e6a3fddbfb81add7effb598779aa3e2edf760
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-07-08 18:02:25 +0300

    Import components directly

--- src/pages/PageOne.vue
+++ src/pages/PageOne.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {TextField} from "@/components";
+import TextField from "@/components/text-field.vue";
 
 export default defineComponent({
   name: "PageOne",
--- src/pages/PageTwo.vue
+++ src/pages/PageTwo.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {Form} from "@/components";
+import Form from "@/components/form.vue";
 
 export default defineComponent({
   name: "PageTwo",
